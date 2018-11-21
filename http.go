0942997609b90a6b1bd031d628503f7387840a61
Jesse Donat <donatj@gmail.com>
2018-11-21 11:49:40 -0600

    Simplify basic auth code signifigantly

--- http.go
+++ http.go
@@ -1,36 +1,17 @@
 package main
 
 import (
-	"encoding/base64"
 	"net/http"
-	"strings"
 )
 
-func BasicAuth(handler http.HandlerFunc, username, password string) http.HandlerFunc {
+func BasicAuth(handler http.Handler, username, password string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		authError := func() {
+		if u, p, ok := r.BasicAuth(); !ok || !(u == username && p == password) {
 			w.Header().Set("WWW-Authenticate", "Basic realm=\"Zork\"")
 			http.Error(w, "authorization failed", http.StatusUnauthorized)
-		}
-
-		auth := strings.SplitN(r.Header.Get("Authorization"), " ", 2)
-		if len(auth) != 2 || auth[0] != "Basic" {
-			authError()
 			return
 		}
 
-		payload, err := base64.StdEncoding.DecodeString(auth[1])
-		if err != nil {
-			authError()
-			return
-		}
-
-		pair := strings.SplitN(string(payload), ":", 2)
-		if len(pair) != 2 || !(pair[0] == username && pair[1] == password) {
-			authError()
-			return
-		}
-
-		handler(w, r)
+		handler.ServeHTTP(w, r)
 	}
 }
